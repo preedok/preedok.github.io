40cc692bea4b7705ed07cb90c3eb711806ce0b47
Hello Dok <edo.vivoy21@gmail.com>
2023-03-23 20:55:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Router from "./router";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  if (loading) {
+    return (
+      <div
+        className="d-flex"
+        style={{
+          backgroundColor: "#041e44",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <img src={require("../src/assets/logo.png")} alt="Logo" />
+      </div>
+    );
+  }
   return (
     <>
       <Router />
